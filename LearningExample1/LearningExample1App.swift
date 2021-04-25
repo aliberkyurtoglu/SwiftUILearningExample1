b038b7263b08cbd23d205ca7de8a537e4fb32480
Ali Berk Yurtoğlu <aliberkyurtoglu@gmail.com>
2021-04-25 14:08:40 +0300

    Initial Commit

---
+++ LearningExample1/LearningExample1App.swift
@@ -0,0 +1,17 @@
+//
+//  LearningExample1App.swift
+//  LearningExample1
+//
+//  Created by Macintosh on 25.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct LearningExample1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
